ed9574840b62205c0b32441599061e2bcc9a07ec
Felipe Rodrigues <felipe_jose_silva@hotmail.com>
2018-07-25 18:04:54 -0300

    first commit

---
+++ src/test/java/com/link/google/test/GoogleTest01.java
@@ -0,0 +1,49 @@
+package com.link.google.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class GoogleTest01 {
+
+
+
+    WebDriver driver;
+
+
+
+    @Test
+    public void Googlet(){
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability(CapabilityType.BROWSER_NAME, BrowserType.FIREFOX);
+        desiredCapabilities.setCapability(CapabilityType.PLATFORM, Platform.LINUX);
+        desiredCapabilities.setCapability("idleTimeout", 150);
+        try {
+
+            //driver.manage().window().maximize();
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),desiredCapabilities);
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        driver.get("http://www.google.com");
+        driver.findElement(By.name("q")).click();
+        driver.findElement(By.name("q")).sendKeys("facebook");
+    }
+
+
+
+
+
+}
